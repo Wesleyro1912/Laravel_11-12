f3735643167a8d7913c814b66422da24de64ed6f
Wesley Rodrigues <127269265+Wesleyro1912@users.noreply.github.com>
2025-09-17 23:08:34 -0300

    feat(estudo): Continuação de estudos

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index($value) {
+        // return view('main', ['value' => $value]);
+
+        // return view('main')->with('value', $value);
+
+        return view('main', compact('value'));
+        
+    }
+}
